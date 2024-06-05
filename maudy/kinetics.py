30e2921de0a5600b6ca737b5e1c735c39081dcb4
carrascomj <carrascomurielj@gmail.com>
2024-06-05 19:47:18 +0200

    fix: correct wrong flux terms

--- maudy/kinetics.py
+++ maudy/kinetics.py
@@ -8,6 +8,7 @@ Matrix = torch.Tensor
 
 
 def get_dgr(S: Matrix, dgf: Vector, water_S: Vector) -> Vector:
+    """Compute the experiment-agnostic dGr (no membrane potential)."""
     return S.T @ dgf + water_S * DGF_WATER
 
 
@@ -18,13 +19,11 @@ def get_saturation(
     sub_conc_idx: list[Vector],
     sub_km_idx: list[Vector],
 ) -> Vector:
-    sub_conc_over_km = torch.cat(
-        [
-            (conc[:, conc_idx] / km[..., km_idx]).prod().reshape(1)
-            for conc_idx, km_idx in zip(sub_conc_idx, sub_km_idx)
-        ]
-    )
-    return free_enzyme_ratio * sub_conc_over_km
+    sub_conc_over_km = torch.cat([
+        (conc[:, conc_idx] / km[..., km_idx]).prod(dim=-1)
+        for conc_idx, km_idx in zip(sub_conc_idx, sub_km_idx)
+    ])
+    return free_enzyme_ratio * sub_conc_over_km.reshape(conc.shape[0], -1)
 
 
 def get_free_enzyme_ratio(
@@ -37,26 +36,22 @@ def get_free_enzyme_ratio(
     substrate_S: list[Vector],
     product_S: list[Vector],
 ) -> Vector:
-    prod_contr = torch.cat(
-        [
-            ((1.0 + (conc[:, conc_idx] / km[..., km_idx])) ** st).prod().reshape(1)
-            - 1.0
-            if km_idx.size(0)
-            else torch.zeros(1)  # if irr: no km for prods: no prod_contr
-            for conc_idx, km_idx, st in zip(prod_conc_idx, prod_km_idx, product_S)
-        ]
-    )
-    sub_contr = torch.cat(
-        [
-            ((1 + (conc[:, conc_idx] / km[..., km_idx])) ** st).prod().reshape(1)
-            for conc_idx, km_idx, st in zip(sub_conc_idx, sub_km_idx, substrate_S)
-        ]
-    ).prod()
-    return 1 / (sub_contr + prod_contr)
+    prod_contr = torch.cat([
+        ((1.0 + (conc[:, conc_idx] / km[..., km_idx])) ** st).prod(dim=-1) - 1.0
+        if km_idx.size(0)
+        else torch.zeros(1)  # if irr: no km for prods: no prod_contr
+        for conc_idx, km_idx, st in zip(prod_conc_idx, prod_km_idx, product_S)
+    ])
+    sub_contr = torch.cat([
+        ((1 + (conc[:, conc_idx] / km[..., km_idx])) ** st).prod(dim=-1)
+        for conc_idx, km_idx, st in zip(sub_conc_idx, sub_km_idx, substrate_S)
+    ])
+    return (1 / (sub_contr + prod_contr)).reshape(conc.shape[0], -1)
 
 
 def get_reversibility(S: Matrix, dgr: Vector, conc: Vector) -> Vector:
-    return 1 - (dgr + RT * conc.log() @ S)
+    """Add the membrane potential to dgr and compute reversibility."""
+    return 1 - torch.exp((dgr + RT * conc.log() @ S) / RT)
 
 
 def get_vmax(kcat: Vector, enzyme_conc: Vector) -> Vector:
